06fd8c22b62fe06f86c48999a281458e9ab6074c
faded20666er <faded20666@gmail.com>
2026-06-15 03:22:25 -0700

    Add watercolor style model configuration

---
+++ AISmokeShaqStudio/models/image/watercolor.js
@@ -0,0 +1,15 @@
+export default {
+  id: "watercolor",
+  name: "Watercolor Style",
+  provider: "huggingface",
+  modelId: "style/watercolor",
+  cost: 1,
+  estTime: "3s",
+  strengths: [
+    "Painterly wash",
+    "Soft edges",
+    "Artistic posters"
+  ],
+  nsfw: false,
+  type: "image"
+};
