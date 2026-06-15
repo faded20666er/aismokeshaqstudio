762891fd81796d8619924b18f401f2755639f4b1
faded20666er <faded20666@gmail.com>
2026-06-15 03:21:45 -0700

    Create disney_style.js

---
+++ AISmokeShaqStudio/models/image/disney_style.js
@@ -0,0 +1,15 @@
+export default {
+  id: "disney-style",
+  name: "Disney Style",
+  provider: "huggingface",
+  modelId: "style/disney",
+  cost: 2,
+  estTime: "4s",
+  strengths: [
+    "Classic animation",
+    "Fairy‑tale look",
+    "Wholesome characters"
+  ],
+  nsfw: false,
+  type: "image"
+};
