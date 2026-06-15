857009ebbe513076da59dc313972f42a35027b79
faded20666er <faded20666@gmail.com>
2026-06-15 02:19:52 -0700

    Create cyberpunk.js

---
+++ AISmokeShaqStudio/models/image/cyberpunk.js
@@ -0,0 +1,15 @@
+export default {
+  id: "cyberpunk",
+  name: "Cyberpunk Style",
+  provider: "huggingface",
+  modelId: "style/cyberpunk",
+  cost: 2,
+  estTime: "4s",
+  strengths: [
+    "Neon lighting",
+    "Futuristic cities",
+    "Sci‑fi vibes"
+  ],
+  nsfw: false,
+  type: "image"
+};
