eac0b91bd99912f2d0e294bb47d1e401b2fa6fe8
faded20666er <faded20666@gmail.com>
2026-06-15 00:36:03 -0700

    Add Juggernaut XL model configuration

---
+++ AISmokeShaqStudio/models/image/juggernaut_xl.js
@@ -0,0 +1,15 @@
+export default {
+  id: "juggernaut-xl",
+  name: "Juggernaut XL",
+  provider: "huggingface",
+  modelId: "runwayml/juggernaut-xl",
+  cost: 3,
+  estTime: "5s",
+  strengths: [
+    "Stylized realism",
+    "Characters",
+    "Fantasy and concept art"
+  ],
+  nsfw: false,
+  type: "image"
+};
