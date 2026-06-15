507da03d6535d5b8918be2ae2293cf5e04941716
faded20666er <faded20666@gmail.com>
2026-06-15 00:17:03 -0700

    Add flux_pro.js model for Flux 1.1 Pro

---
+++ AISmokeShaqStudio/models/image/flux_pro.js
@@ -0,0 +1,15 @@
+export default {
+  id: "flux-pro",
+  name: "Flux 1.1 Pro",
+  provider: "replicate",
+  modelId: "black-forest-labs/flux-1.1-pro",
+  cost: 5,              // premium image
+  estTime: "6s",
+  strengths: [
+    "Photorealism",
+    "Portraits",
+    "Cinematic"
+  ],
+  nsfw: false,
+  type: "image"
+};
