ec25998a26f70c723a5f68702f3dd85f167ccbb9
faded20666er <faded20666@gmail.com>
2026-06-15 02:06:15 -0700

    Add DreamShaper XL model configuration

---
+++ AISmokeShaqStudio/models/image/dreamshaper_xl.js
@@ -0,0 +1,15 @@
+export default {
+  id: "dreamshaper-xl",
+  name: "DreamShaper XL",
+  provider: "huggingface",
+  modelId: "Lykon/dreamshaper-xl",
+  cost: 2,
+  estTime: "4s",
+  strengths: [
+    "Fantasy art",
+    "Illustrations",
+    "Concept art"
+  ],
+  nsfw: false,
+  type: "image"
+};
