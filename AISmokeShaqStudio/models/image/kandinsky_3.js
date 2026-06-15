c0ac0651f6ce726e50a1e80630c5747d58a7ad35
faded20666er <faded20666@gmail.com>
2026-06-15 02:07:49 -0700

    Add Kandinsky 3 model configuration

---
+++ AISmokeShaqStudio/models/image/kandinsky_3.js
@@ -0,0 +1,15 @@
+export default {
+  id: "kandinsky-3",
+  name: "Kandinsky 3",
+  provider: "huggingface",
+  modelId: "kandinsky-community/kandinsky-3",
+  cost: 2,
+  estTime: "4s",
+  strengths: [
+    "Artistic",
+    "Painterly styles",
+    "Abstract compositions"
+  ],
+  nsfw: false,
+  type: "image"
+};
