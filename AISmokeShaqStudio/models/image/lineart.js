8fca0a4bbdf4b106a173fd450588cf7755eba3bd
faded20666er <faded20666@gmail.com>
2026-06-15 03:23:22 -0700

    Create lineart.js

---
+++ AISmokeShaqStudio/models/image/lineart.js
@@ -0,0 +1,15 @@
+export default {
+  id: "lineart",
+  name: "Line Art",
+  provider: "huggingface",
+  modelId: "style/lineart",
+  cost: 1,
+  estTime: "3s",
+  strengths: [
+    "Clean outlines",
+    "Comics",
+    "Sketch‑style visuals"
+  ],
+  nsfw: false,
+  type: "image"
+};
