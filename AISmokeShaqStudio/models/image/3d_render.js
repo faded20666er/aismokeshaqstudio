9be93670612e956c1b1aedb4e48380f1fa4b0e5a
faded20666er <faded20666@gmail.com>
2026-06-15 02:19:09 -0700

    Create 3d_render.js

---
+++ AISmokeShaqStudio/models/image/3d_render.js
@@ -0,0 +1,15 @@
+export default {
+  id: "3d-render",
+  name: "3D Render",
+  provider: "huggingface",
+  modelId: "3d-render/3d-style",
+  cost: 2,
+  estTime: "4s",
+  strengths: [
+    "3D look",
+    "CGI style",
+    "Game assets"
+  ],
+  nsfw: false,
+  type: "image"
+};
