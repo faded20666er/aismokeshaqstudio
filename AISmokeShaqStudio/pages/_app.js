c5a486c60c974268c28e77598a2318c129970cc9
faded20666er <faded20666@gmail.com>
2026-06-18 11:04:42 -0700

    Initialize main app component with global styles

---
+++ AISmokeShaqStudio/pages/_app.js
@@ -0,0 +1,17 @@
+import "../styles/globals.css";
+import Head from "next/head";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
+}
