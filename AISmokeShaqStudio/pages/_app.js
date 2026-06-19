86e7501bc439d4ebd68bc19c5f073afbe57c9880
faded20666er <faded20666@gmail.com>
2026-06-18 23:26:15 -0700

    Update _app.js

--- AISmokeShaqStudio/pages/_app.js
+++ AISmokeShaqStudio/pages/_app.js
@@ -1,17 +1,6 @@
-import "../styles/globals.css";
-import Head from "next/head";
+// pages/_app.js
+import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
